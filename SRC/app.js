9af3c8d462a60fa07dd8d17d9f3caa5d60b318b6
affan-11 <affanarif4@gmail.com>
2022-05-23 11:21:34 +0100

    what you changed

---
+++ SRC/app.js
@@ -0,0 +1,14 @@
+const {addMovie} = require("./utils");
+
+console.log(addMovie({title: "Spiderman", actor: "Andrew Garfield"}));
+
+
+
+// Accept at least movie title on command line
+
+
+// If user input has actor, capture this aswell
+
+// Add movie object to an array
+
+// Output movie/ movies to console
---
+++ SRC/utils/index.js
@@ -0,0 +1,5 @@
+exports.addMovie = (movieObj) => {
+    //take a movieObj, add it to an array, then return the array
+    return [movieObj];
+}
+
